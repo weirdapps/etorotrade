2a54472d429cf7acc5087c17fb65b096cd5b8be8
Dimitris <plessasdimitrios@yahoo.com>
2024-07-14 04:21:09 +0300

    refactor env, request, fetch data, format

---
+++ support/load_env.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+def load_environment():
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API key not found. Please make sure it's added to the .env file.")
+    return api_key
