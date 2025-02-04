43c588fb86e733d66b67cd5721b1bcc62aa6e727
Dimitris <plessasdimitrios@yahoo.com>
2025-02-04 03:59:32 +0200

    fix color coding

--- yfinance/price.py
+++ yfinance/price.py
@@ -1,7 +1,8 @@
-## price.py
 import yfinance as yf
 
-def get_current_price(ticker_symbol):
-    stock = yf.Ticker(ticker_symbol)
+def get_current_price(ticker):
+    """Fetch the latest closing price of a stock."""
+    stock = yf.Ticker(ticker)
     data = stock.history(period="1d")
+
     return data['Close'].iloc[-1] if not data.empty else None
